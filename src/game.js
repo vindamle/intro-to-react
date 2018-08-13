be33e73887d899479a31380cc9351c5959c6b46a
vindamle <vindamle@yahoo.com>
2018-08-13 16:58:48 -0400

    'Lifting State Up, Again' changes

--- src/game.js
+++ src/game.js
@@ -1,20 +1,60 @@
 import React from 'react';
-import './board.css';
+import './game.css';
 import Board from './board';
+import calculateWinner from './calculatewinner';
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+        };
+    }
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner  = calculateWinner(current.squares);
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{ status }</div>
                     <ol>{ /* TODO */ }</ol>
                 </div>
             </div>
         );
+    }
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            xIsNext: !this.state.xIsNext,
+        });
     }
 }
 
