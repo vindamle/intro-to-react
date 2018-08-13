29c5b7b06aa1ea26c0aec155d59af6b913c9265b
vindamle <vindamle@yahoo.com>
2018-08-13 15:43:17 -0400

    'Functional Components' changes

--- src/square.js
+++ src/square.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import './square.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={ () => this.props.onClick()}
-            >
-                {this.props.value}
+function Square(props) {
+    return (
+            <button className="square" onClick={ props.onClick }>
+                {props.value}
             </button>
-        );
-    }
+    );
 }
 
-export default Square
+export default Square;
