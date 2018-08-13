1df9d5fc5ae58b9226033ab437d79319cc0b94b2
vindamle <vindamle@yahoo.com>
2018-08-13 15:06:52 -0400

    'Making an Interactive Component' changes

--- src/square.js
+++ src/square.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './square.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square"
+                 onClick={ () => this.setState({value: 'X'}) }>
+                {this.state.value}
             </button>
         );
     }
